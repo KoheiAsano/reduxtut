e1693a73937b707041768d05aceb4faa53e20b68
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-15 06:53:48 +0900

    add firebase configure

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAc0ScMpyrQLcrwsya63ZLHG2oNGSWJ_ng",
+  authDomain: "redu-todo.firebaseapp.com",
+  databaseURL: "https://redu-todo.firebaseio.com",
+  projectId: "redu-todo",
+  storageBucket: "redu-todo.appspot.com",
+  messagingSenderId: "567478814384"
+}
