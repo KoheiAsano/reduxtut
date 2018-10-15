88b34669a44823ea6f6d7f0b87b40ab6af611ac5
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-15 17:02:19 +0900

    implement hover

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import * as actions from '../actions'
 
 import Button from '@material-ui/core/Button'
+import { StyleSheet, css } from 'aphrodite';
+
 
 const AddTodo = ({ addTodo }) => {
   let input
@@ -19,20 +21,40 @@ const AddTodo = ({ addTodo }) => {
           input.value = ''
         }}
       >
-        <input ref={node => input = node} />
-        <Button type="submit"
-                style={{
-                  color: 'white',
-                  backgroundColor: 'lightGreen',
-                  marginLeft: '1px',
-                  width:"120px",
-                  height:"2px",
-                }}
+        <input className={css(styles.hover)} ref={node => input = node} />
+        <button className={css(styles.hover)}
+        type="submit"
+
         >
           Add Todo
-        </Button>
+        </button>
       </form>
     </div>
   )
 }
 export default connect(null,actions)(AddTodo)
+
+const styles = StyleSheet.create({
+    red: {
+        backgroundColor: 'red'
+    },
+
+    blue: {
+        backgroundColor: 'blue'
+    },
+
+    hover: {
+        color: 'white',
+        backgroundColor: 'lightGreen',
+        marginLeft: '1px',
+        ':hover': {
+            backgroundColor: 'red'
+        }
+    },
+
+    small: {
+        '@media (max-width: 600px)': {
+            backgroundColor: 'red',
+        }
+    }
+});
