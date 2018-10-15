5b4fefd70bbbb2a7830be75274b337f5ab671f6f
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-15 10:18:12 +0900

    remove keys

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,2 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAc0ScMpyrQLcrwsya63ZLHG2oNGSWJ_ng",
-  authDomain: "redu-todo.firebaseapp.com",
-  databaseURL: "https://redu-todo.firebaseio.com",
-  projectId: "redu-todo",
-  storageBucket: "redu-todo.appspot.com",
-  messagingSenderId: "567478814384"
-}
+import keys from './keys';
+export const firebaseConfig = keys;
