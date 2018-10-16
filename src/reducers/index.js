6c4fb7dcd346c7e3040d92f2f60cca92350ac8bd
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-16 12:51:07 +0900

    recombine visibilityfilter

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,6 @@ import visibilityFilter from './visibilityFilter'
 import {firebaseReducer} from 'react-redux-firebase'
 
 export default combineReducers({
+  visibilityFilter,
   firebase: firebaseReducer,
-  // todos,
-  // visibilityFilter
 })
