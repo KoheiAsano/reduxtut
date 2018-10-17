b279d95921f9e0c270017c6cad5c82398eef6a79
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-17 11:04:19 +0900

    implement image props, and rendering way

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -13,25 +13,39 @@ const theme = createMuiTheme({
     useNextVariants: true,
   },
 });
+const createObjectURL = (window.URL || window.webkitURL).createObjectURL || window.createObjectURL;
 
-const Todo = ({ onClick, completed, text}) => (
-  <MuiThemeProvider theme={theme}>
-    <ListItem key={text} dense button >
-    <div>
-      <img src={'/alok.jpg'} style={{width: 50,height:50,}}/>
-      <input type="file" accept="image" name="icon" onChange={() => console.log()}/>
-    </div>
-      <ListItemText primary={text} />
-      <ListItemSecondaryAction >
-        <Checkbox
-          onChange={onClick}
-          checked={completed}
-        />
-      </ListItemSecondaryAction>
-    </ListItem>
-  </MuiThemeProvider>
-)
+const handleChangeFile = (e) => {
+    let files = e.target.files;
+    let image_url = createObjectURL(files[0]);
+    // return image_url;
+}
 
+
+        // <div>
+        //
+        //     <img src={this.state.image_src} />
+        //     <button onClick={this.clickPostBtn} type="button">投稿する</button>
+        // </div>
+const Todo = ({ onClick, completed, text, img}) => {
+  let iconSrc = '/alok.jpg'
+  return(
+    <MuiThemeProvider theme={theme}>
+      <ListItem key={text} dense button >
+        <div>
+          <img alt={'NO IMG'} src={img} style={{width: 50,height:50,}}/>
+        </div>
+        <ListItemText primary={text} />
+        <ListItemSecondaryAction >
+          <Checkbox
+            onChange={onClick}
+            checked={completed}
+          />
+        </ListItemSecondaryAction>
+      </ListItem>
+    </MuiThemeProvider>
+  )
+}
 Todo.propTypes = {
   onClick: PropTypes.func.isRequired,
   text: PropTypes.string.isRequired
